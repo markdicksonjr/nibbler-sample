ab771c90565def360004312e9752205ca01bcb57
Mark Dickson Jr <markdicksonjr@gmail.com>
2020-02-12 10:52:27 -0500

    Made sample app do slightly more
    It shows off the session and sql extensions

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/markdicksonjr/nibbler-sample
 go 1.12
 
 require (
-	github.com/markdicksonjr/nibbler v0.14.0
+	github.com/markdicksonjr/nibbler v0.15.2
 	github.com/markdicksonjr/nibbler-mail-outbound v0.0.3
-	github.com/markdicksonjr/nibbler-sql v0.0.3
+	github.com/markdicksonjr/nibbler-sql v0.0.4
 	github.com/wader/gormstore v0.0.0-20181020092115-137864aca927 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/markdicksonjr/nibbler"
 	"github.com/markdicksonjr/nibbler-mail-outbound/sendgrid"
 	sql "github.com/markdicksonjr/nibbler-sql"
@@ -15,15 +16,82 @@ import (
 
 type Extension struct {
 	nibbler.NoOpExtension
-	AuthExtension *local.Extension
+	AuthExtension    *local.Extension
+	SessionExtension *session.Extension
+	UserExtension    *user.Extension
 }
 
 func (s *Extension) GetName() string {
 	return "sample"
 }
 
+type AppSpecificContext struct {
+	Count int
+}
+
 func (s *Extension) PostInit(context *nibbler.Application) error {
-	context.Router.HandleFunc("/api/ok", s.AuthExtension.EnforceLoggedIn(func(w http.ResponseWriter, _ *http.Request) {
+
+	// this adds the route "/api/ok" which increments a counter in protected context and a different counter in context
+	// after hitting this endpoint, send a request to "/api/user" to ensure you don't see the protected context but
+	// see the user-visible context.  Log out, log in, notice the result gets restored from the db
+	context.Router.HandleFunc("/api/ok", s.AuthExtension.EnforceLoggedIn(func(w http.ResponseWriter, r *http.Request) {
+
+		// get the user from the session
+		caller, err := s.SessionExtension.GetCaller(r)
+		if err != nil {
+			nibbler.Write500Json(w, err.Error())
+			return
+		}
+		if caller == nil {
+			nibbler.Write404Json(w)
+			return
+		}
+
+		// get the protected context on the caller, increment a counter
+		var protectedContext AppSpecificContext
+		if caller.ProtectedContext != nil {
+			if err := json.Unmarshal([]byte(*caller.ProtectedContext), &protectedContext); err != nil {
+				nibbler.Write500Json(w, err.Error())
+				return
+			}
+		}
+		protectedContext.Count++
+
+		// convert ctx to string, write it to protected context
+		pc, _ := json.Marshal(protectedContext)
+		if pc != nil {
+			pcS := string(pc)
+			caller.ProtectedContext = &pcS
+		}
+
+		// get the protected context on the caller, increment a counter
+		var ctx AppSpecificContext
+		if caller.Context != nil {
+			if err := json.Unmarshal([]byte(*caller.Context), &ctx); err != nil {
+				nibbler.Write500Json(w, err.Error())
+				return
+			}
+		}
+		ctx.Count+=2
+
+		c, _ := json.Marshal(ctx)
+		if c != nil {
+			cS := string(c)
+			caller.Context = &cS
+		}
+
+		// save the user back to the session
+		if err := s.SessionExtension.SetCaller(w, r, caller); err != nil {
+			nibbler.Write500Json(w, err.Error())
+			return
+		}
+
+		// save the user back to the database
+		if err := s.UserExtension.Update(caller); err != nil {
+			nibbler.Write500Json(w, err.Error())
+			return
+		}
+
 		w.WriteHeader(http.StatusOK)
 		w.Header().Set("Content-Type", "application/json")
 		w.Write([]byte(`{"result": "OK"}`))
@@ -88,6 +156,12 @@ func main() {
 		EmailVerificationRedirect:  "http://localhost:3000/#/verify-email",
 	}
 
+	sampleExtension := Extension{
+		AuthExtension:    &userLocalAuthExtension,
+		SessionExtension: &sessionExtension,
+		UserExtension:    &userExtension,
+	}
+
 	// initialize the application
 	appContext := nibbler.Application{}
 	if err := appContext.Init(config, nibbler.DefaultLogger{}, []nibbler.Extension{
@@ -96,9 +170,7 @@ func main() {
 		&sessionExtension,
 		&userLocalAuthExtension,
 		&sendgridExtension,
-		&Extension{
-			AuthExtension: &userLocalAuthExtension,
-		},
+		&sampleExtension,
 	}); err != nil {
 		log.Fatal(err.Error())
 	}
